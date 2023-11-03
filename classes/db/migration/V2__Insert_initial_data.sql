45145d2c60622a25e2a3513ffdcf6fb99dbd8a51
HaoZengGH <91028444+HaoZengGH@users.noreply.github.com>
2023-11-02 18:36:54 -0700

    Delete classes directory

--- classes/db/migration/V2__Insert_initial_data.sql
+++
@@ -1,9 +0,0 @@
--- Doctor Table Data
-INSERT INTO doctor(id, first_name, last_name) VALUES (1, 'John', 'Doe');
-INSERT INTO doctor(id, first_name, last_name) VALUES (2, 'Jane', 'Smith');
-INSERT INTO doctor(id, first_name, last_name) VALUES (3, 'Lucy', 'Hart');
-
--- Appointment Table Data (assuming some sample data for simplicity)
-INSERT INTO appointment(id, doctor_id, patient_first_name, patient_last_name, appointment_date_time, kind) VALUES (1, 1, 'Alan', 'Brown', '2023-10-22 08:15:00', 'New Patient');
-INSERT INTO appointment(id, doctor_id, patient_first_name, patient_last_name, appointment_date_time, kind) VALUES (2, 1, 'Sara', 'Wilson', '2023-10-22 09:00:00', 'Follow-up');
-INSERT INTO appointment(id, doctor_id, patient_first_name, patient_last_name, appointment_date_time, kind) VALUES (3, 2, 'George', 'Turner', '2023-10-22 10:30:00', 'New Patient');
